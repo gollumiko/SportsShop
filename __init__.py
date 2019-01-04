9c980e1d0177f3f01627eb3b5cbf0e5c9a9a61f6
gollumiko <gollumiko@gmail.com>
2019-01-04 18:34:28 +0100

    removed duplicate

--- __init__.py
+++ __init__.py
@@ -20,13 +20,6 @@ def init_db():
         db.commit()
 
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
