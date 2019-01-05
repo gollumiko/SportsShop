c8fa3741dfc73dcd69de09176c4f4eaa95efacd5
gollumiko <gollumiko@gmail.com>
2019-01-05 17:42:16 +0100

    basic database access operations

--- __init__.py
+++ __init__.py
@@ -20,6 +20,36 @@ def init_db():
         db.commit()
 
 
+
+
+def get_all_categories(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM category")     
+    rows = cur.fetchall()
+    categories = []
+    for row in rows:
+        category = {}
+        category['id'] = row[0]
+        category['description'] = row[1]
+        category['image'] = row[2]
+        categories.append(category)
+    return categories
+     
+def get_articles(conn, category_id):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM article WHERE categoryid=" + category_id)     
+    rows = cur.fetchall()
+    articles = []
+    for row in rows:
+        article = {}
+        article['id'] = row[0]
+        article['description'] = row[1]
+        article['price'] = row[2]
+        article['image'] = row[3]
+        articles.append(article)
+    return articles
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -30,6 +60,17 @@ def close_connection(exception):
 def home():
     return ""
 
+@app.route("/categories/")
+def getCategories():
+    conn = get_db()
+    categories = get_all_categories(conn)
+    return json.dumps(categories, ensure_ascii=False)
+
+@app.route("/categories/<category_id>/articles/")
+def getArticles(category_id):
+    conn = get_db()
+    articles = get_articles(conn, category_id)
+    return json.dumps(articles, ensure_ascii=False)
 
 
 if __name__ == "__main__":
